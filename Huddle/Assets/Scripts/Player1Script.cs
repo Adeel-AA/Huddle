6c0c0efc59032b6d38598d9bed82cbfdebc94261
zamanp <zamanp@aston.ac.uk>
2017-03-14 15:44:27 +0000

    tried to implement the unstuck feature for the box

--- Huddle/Assets/Scripts/Player1Script.cs
+++ Huddle/Assets/Scripts/Player1Script.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Player1Script : MonoBehaviour
 {
 
     public Vector2 speed = new Vector2(50, 50);
+	private Vector3 movement;
+	private bool isboxStuck;
+	private Collision2D boxStuck;
 
     // Update is called once per frame
     void Update()
@@ -18,5 +22,20 @@ public class Player1Script : MonoBehaviour
         movement *= Time.deltaTime;
 
         transform.Translate(movement);
+		try {boxStuck.transform.Translate (movement);}
+		catch (Exception e) {
+		}
     }
+	// drag object
+	void OnCollisionEnter2D(Collision2D box) {
+
+		 
+		if (box.gameObject.layer == 18 || box.gameObject.layer == 19) {
+				Debug.Log ("the correct box is here");
+				isboxStuck = true;
+				boxStuck = box;
+        }
+		}
+
+
 }
