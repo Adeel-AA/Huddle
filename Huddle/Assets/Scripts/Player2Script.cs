0c2908f3e6157df2ee61e7f8ba9a6f497cdcae50
zamanp <zamanp@aston.ac.uk>
2017-03-17 22:47:58 +0000

    Level 6

--- Huddle/Assets/Scripts/Player2Script.cs
+++ Huddle/Assets/Scripts/Player2Script.cs
@@ -1,45 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System;
 
 public class Player2Script : MonoBehaviour
 {
 
     public Vector2 speed = new Vector2(50, 50);
-	private Vector3 movement;
-	private bool isboxStuck;
-	private Collision2D boxStuck;
 
     // Update is called once per frame
     void Update()
     {
-		if (isboxStuck != false) {
-			if (Input.GetKey (KeyCode.RightShift)) {
-				boxStuck = null;
-			}
-		}
+
         float inputX = Input.GetAxis("2Horizontal");
         float inputY = Input.GetAxis("2Vertical");
 
-        movement = new Vector3(speed.x * inputX, speed.y * inputY);
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY);
 
         movement *= Time.deltaTime;
 
         transform.Translate(movement);
-		try {boxStuck.transform.Translate (movement);}
-		catch (Exception e) {
-		}
     }
-	void OnCollisionEnter2D(Collision2D box) {
-
-
-		if (box.gameObject.layer == 13 || box.gameObject.layer == 19) {
-
-			Debug.Log ("the correct box is here");
-			isboxStuck = true;
-			boxStuck = box;
-
-
-		}
-}
 }
--- Huddle/Assets/Scripts/SavedState.cs
+++ Huddle/Assets/Scripts/SavedState.cs
@@ -6,7 +6,6 @@ using System;
 public class SavedState {
 	private  DataTable table;
 	private DataSet ds;
-	private static string savefile = Application.dataPath + "/SaveFiles/save.xml";
 
 
 
@@ -28,7 +27,7 @@ public class SavedState {
 	}
 
 	public void flushSave (string playerName,int level, int score) {
-		try {ds.ReadXml (savefile);
+		try {ds.ReadXml (Application.dataPath + "/SaveFiles/save.xml");
 			// clears table of any level duplicates 
 			for (int i = 0; i < ds.Tables [0].Rows.Count; i++) {
 				if (ds.Tables [0].Rows [i] ["Name"].ToString ().Equals (playerName) && ds.Tables [0].Rows [i] ["Level"].Equals(level)) {
@@ -52,10 +51,6 @@ public class SavedState {
 
 	// need to implement this too tiered today lol
 	public void readSave () {
-		try { ds.ReadXml (Application.dataPath +savefile); }
-		catch (Exception e) {}
-
-
 
 	}
 
